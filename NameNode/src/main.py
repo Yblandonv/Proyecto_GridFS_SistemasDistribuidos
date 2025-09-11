b3a619757019fcf417bbf4328d4bef27368f8c8a
jotaPe-dv <jpruac@eafit.edu.co>
2025-09-11 11:10:34 -0500

    merge

--- NameNode/src/main.py
+++
@@ -1,8 +0,0 @@
-import grpc
-import os
-from dotenv import load_dotenv
-from nameNode import serve
-
-if __name__ == "__main__":
-    load_dotenv()
-    serve()
--- NameNode/src/nameNode.py
+++ NameNode/src/nameNode.py
@@ -1,8 +1,9 @@
 import grpc
 
 from concurrent import futures
-
 from NameNode.stubs import servicios_pb2, servicios_pb2_grpc
+
+
 
 class cliente_nameServicer(servicios_pb2_grpc.cliente_nameServicer):
     def enviar_metadata(self, request, context):
@@ -15,6 +16,8 @@ def recibir_peticiones():
     server.add_insecure_port("[::]:8080")
     server.start()
     server.wait_for_termination()
+
+
 
 
 if __name__ == "__main__":
