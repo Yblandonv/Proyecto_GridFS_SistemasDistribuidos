1b9f6e0cfb315baaa2484d7d0ddaea6b49b4d221
jotaPe-dv <jpruac@eafit.edu.co>
2025-09-11 13:36:49 -0500

    guardado de bloques y creacion de tabla ip datanodes

--- NameNode/src/nameNode.py
+++ NameNode/src/nameNode.py
@@ -8,6 +8,7 @@ from NameNode.stubs import servicios_pb2, servicios_pb2_grpc
 
 class cliente_nameServicer(servicios_pb2_grpc.cliente_nameServicer):
     def enviar_metadata(self, request, context):
+        guardar_bloques(request.nombre_archivo, request.numero_bloques)
         return servicios_pb2.metadata(message=f"Hola, {request.nombre_archivo} {request.numero_bloques}!")
 
 
@@ -34,10 +35,30 @@ def connect_database():
             sqlite_connection.close()
             print('SQLite Connection closed')
     
+def guardar_bloques(nombre_archivo, numero_bloques):
+    conn = sqlite3.connect('bloques.db')
+    cursor = conn.cursor()
+    for i in range(numero_bloques):
+        cursor.execute("INSERT INTO bloques (name_block, id_block) VALUES (?, ?)", (nombre_archivo, i))
+    conn.commit()
+    cursor.close()
 
+def agregar_datanode(ip):
+    conn = sqlite3.connect('bloques.db')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO datanodes (ip) VALUES (?)", (ip,))
+    conn.commit()
+    cursor.close()
+    conn.close()
 
-
-
+def obtener_datanodes():
+    conn = sqlite3.connect('bloques.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, ip FROM datanodes")
+    datanodes = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return datanodes
 
 if __name__ == "__main__":
     connect_database()
